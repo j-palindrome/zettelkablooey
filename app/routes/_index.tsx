2a0f6989b1da9fb73deec640f99a3faf6106a6a3
Joshua Tazman Reinier <jreinier@oberlin.edu>
2023-06-30 10:17:49 -0700

    initial scene

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,24 +1,8 @@
-import { Container, Sprite, Stage, Text } from '@pixi/react'
-import { useSpring, animated, useSprings } from '@react-spring/web'
-import { BlurFilter, TextStyle } from 'pixi.js'
-import { Suspense, useEffect, useMemo, useRef, useState } from 'react'
-import { ClientOnly } from 'remix-utils'
-import { defaultStyle } from '~/models/text'
-import { repulsiveBody, useDimensions, useMatter } from '~/utils'
-import Matter, { Bodies, Body, Vector, Constraint } from 'matter-js'
+import { animated, useSpring, useSprings } from '@react-spring/web'
 import _ from 'lodash'
-import {
-  b2BodyType,
-  b2PolygonShape,
-  b2Shape,
-  b2Vec2,
-  b2World,
-} from '@box2d/core'
-import invariant from 'tiny-invariant'
-import stylesheet from '~/tailwind.css'
-import { Render } from 'react-matter-js'
-// @ts-ignore
-import attractors from 'matter-attractors'
+import { Vector } from 'matter-js'
+import { Suspense, useEffect, useRef, useState } from 'react'
+import { useDimensions, useFlicker, useFlickers } from '~/utils'
 
 export default function Index() {
   return (
@@ -28,14 +12,21 @@ export default function Index() {
   )
 }
 
-Matter.use(attractors)
-
 function ClientIndex() {
   const [data, setData] = useState<{ event: 'start' | 'explode' }>({
     event: 'start',
   })
 
-  const { w, h } = useDimensions()
+  const { w } = useDimensions()
+
+  const titleRef = useRef<HTMLButtonElement>(null)
+  useFlicker(titleRef, {
+    go: data.event === 'explode',
+    key: 'opacity',
+    from: { max: 1, min: 0 },
+    to: { max: 0, min: 0 },
+    duration: 600,
+  })
 
   return (
     <div className='relative h-full w-full child:absolute child:-translate-x-1/2 child:-translate-y-1/2'>
@@ -44,7 +35,10 @@ function ClientIndex() {
           data.event === 'start' && setData({ ...data, event: 'explode' })
         }
         className='left-1/2 top-1/2'
-        style={{ fontSize: ((w * 2) / 'zettelkablooey'.length) * 0.25 }}
+        style={{
+          fontSize: ((w * 2) / 'zettelkablooey'.length) * 0.25,
+        }}
+        ref={titleRef}
       >
         zettelkablooey
       </button>
@@ -55,16 +49,33 @@ function ClientIndex() {
 
 function Explosion() {
   const { w, h } = useDimensions()
-  const text: { t: string; w: number; s: React.CSSProperties }[] = [
+  const text: {
+    t: string
+    w: number
+    s: React.CSSProperties
+    id: string
+    o: number
+  }[] = [
     {
       t: 'zettelkasten: a moment of space between offerings',
-      w: 100,
-      s: { fontSize: 12, opacity: 0.7 },
+      w: 150,
+      s: { fontSize: 12 },
+      o: 1,
+      id: 'luhmann',
     },
     {
       t: 'Auf diese Frage kann hier nicht deduktiv, nicht aus einer Obersicht über alle Moglichkeiten und nicht durch Auswahl der besten geantwortet werden.',
       w: 400,
-      s: { fontSize: 10, opacity: 0.5 },
+      s: { fontSize: 10 },
+      o: 1,
+      id: 'frage',
+    },
+    {
+      t: 'Verweisungsmoglichkeiten',
+      w: 200,
+      s: { fontSize: 18 },
+      o: 1,
+      id: 'verweisungs',
     },
   ]
 
@@ -72,120 +83,33 @@ function Explosion() {
     text.length,
     (i) => ({
       from: Vector.create(w / 2, h / 2),
-      to: Vector.add(
-        Vector.create(w / 2, h / 2),
-        Vector.rotate(
-          Vector.create(0, _.random(50, w * 0.5 - )),
-          _.random(Math.PI * 2, true)
-        )
-      ),
+      to: Vector.create(_.random(w - text[i].w, true), _.random(h - 50, true)),
       config: {
-        damping: 100,
-        friction: 5 + _.random(5.0),
-        tension: 50 * _.random(0.8, 1.2),
+        damping: 500,
+        friction: 10 * _.random(0.9, 1.1),
+        tension: 200 * _.random(0.8, 1.2),
       },
     }),
     []
   )
 
-  // const grounds = useMemo(
-  //   () => [
-  //     Bodies.rectangle(w / 2, h - 5, w * 1.5, 10, {
-  //       isStatic: true,
-  //       label: 'ground-b',
-  //     }),
-  //     Bodies.rectangle(w / 2, 5, w * 1.5, 10, {
-  //       isStatic: true,
-  //       label: 'ground-t',
-  //     }),
-  //     Bodies.rectangle(5, h / 2, 10, h * 1.5, {
-  //       isStatic: true,
-  //       label: 'ground-l',
-  //     }),
-  //     Bodies.rectangle(w - 5, h / 2, 10, h * 1.5, {
-  //       isStatic: true,
-  //       label: 'ground-r',
-  //     }),
-  //   ],
-  //   []
-  // )
-
-  // const texts = useMemo(
-  //   () =>
-  //     text.map((text) => {
-  //       const body = Bodies.rectangle(
-  //         w / 2 + _.random(30),
-  //         h / 2 + _.random(30),
-  //         25,
-  //         50,
-  //         {
-  //           label: text.id,
-  //         }
-  //       )
-
-  //       const vector = Vector.rotate(
-  //         Vector.create(-0.05, 0),
-  //         _.random(Math.PI, true)
-  //       )
-  //       Body.applyForce(body, body.position, vector)
-
-  //       return body
-  //     }),
-  //   []
-  // )
-
-  // useMatter(
-  //   [
-  //     ...grounds,
-  //     ...texts,
-  //     ...texts.flatMap((text) =>
-  //       [
-  //         { x: 0, y: 0 },
-  //         { x: w, y: 0 },
-  //         { x: w, y: h },
-  //         { x: 0, y: h },
-  //       ]
-  //         .map((point) =>
-  //           Constraint.create({
-  //             bodyA: text,
-  //             pointB: point,
-  //             length: h,
-  //             stiffness: 1e-4,
-  //             damping: 1e-4,
-  //           })
-  //         )
-  //         .concat([
-  //           Constraint.create({
-  //             bodyA: text,
-  //             pointB: { x: w / 2, y: h / 2 },
-  //             length: h / 4,
-  //             stiffness: 1e-4,
-  //             damping: 1e-4,
-  //           }),
-  //         ])
-  //     ),
-  //   ],
-  //   () => {
-  //     for (let body of texts) {
-  //       const el = document.getElementById(body.label)
-  //       invariant(el)
-  //       el.style.top = `${body.position.y}px`
-  //       el.style.left = `${body.position.x}px`
-  //     }
-  //   },
-  //   (world, engine) => {
-  //     world.gravity.x = 0
-  //     world.gravity.y = 0
-  //     const render = Matter.Render.create({ engine })
-  //     Matter.Render.run(render)
-  //   }
-  // )
+  useFlickers(
+    text.map((fragment) => '#' + fragment.id),
+    (i) => ({
+      from: { min: 0, max: text[i].o },
+      to: { min: text[i].o, max: text[i].o },
+      duration: 2000 * _.random(0.8, 1.2),
+      key: 'opacity',
+      go: true,
+    })
+  )
 
   return (
     <>
       {text.map((fragment, i) => (
         <animated.div
           key={fragment.t}
+          id={fragment.id}
           style={{ width: fragment.w, ...fragment.s, ...springs[i] }}
         >
           {fragment.t}
--- app/utils.ts
+++ app/utils.ts
@@ -1,6 +1,8 @@
 import { b2Body, b2BodyType, b2Vec2, b2World } from '@box2d/core'
 import { useMatches, useOutletContext } from '@remix-run/react'
 import { RemixContext } from '@remix-run/react/dist/components'
+import _ from 'lodash'
+import math from 'mathjs'
 import Matter, {
   Bodies,
   Engine,
@@ -13,7 +15,15 @@ import Matter, {
   IEngineDefinition,
   Vector,
 } from 'matter-js'
-import { useEffect, useMemo, useRef, useState } from 'react'
+import p5 from 'p5'
+import {
+  RefObject,
+  StyleHTMLAttributes,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
 import invariant from 'tiny-invariant'
 
 const DEFAULT_REDIRECT = '/'
@@ -86,4 +96,91 @@ export const repulsiveBody = (max_distance: number, scale: number) => {
       Math.max(0, max_distance - Vector.magnitude(diff)) * scale
     )
   }
+}
+
+/**
+ * @param el: either a document.querySelector string or a ref to an HTMLElement.
+ */
+export const useFlicker = (
+  el: RefObject<HTMLElement> | string,
+  {
+    go,
+    key,
+    from,
+    to,
+    duration,
+    units,
+  }: {
+    go: boolean
+    key: keyof CSSStyleDeclaration
+    from: { max: number; min: number }
+    to: { max: number; min: number }
+    duration: number
+    units?: 'px' | 'vw' | 'vh' | '%' | ((amount: number) => string)
+  }
+) => {
+  const currentFlicker = useRef<{ max: number; min: number }>({ ...from })
+  const startTime = useRef<number>()
+
+  useEffect(() => {
+    const flicker: FrameRequestCallback = (currentTime: number) => {
+      if (!startTime.current) startTime.current = currentTime
+
+      const element =
+        typeof el === 'string'
+          ? (document.querySelector(el) as HTMLElement)
+          : el.current
+
+      invariant(element)
+      const amount = _.round(
+        _.random(currentFlicker.current.min, currentFlicker.current.max, true),
+        3
+      )
+
+      const unitAmount =
+        typeof units === 'function' ? units(amount) : `${amount}${units ?? ''}`
+      element.style.setProperty(key as string, unitAmount)
+
+      const progress = (currentTime - startTime.current) / duration
+
+      currentFlicker.current.max = lerp(from.max, to.max, progress)
+      currentFlicker.current.min = lerp(from.min, to.min, progress)
+      if (progress > 1.0) return
+      if (go) requestAnimationFrame(flicker)
+    }
+
+    if (go) {
+      requestAnimationFrame(flicker)
+    }
+  }, [go])
+}
+
+export const useFlickers = (
+  els: Parameters<typeof useFlicker>[0][],
+  config:
+    | Parameters<typeof useFlicker>[1]
+    | Parameters<typeof useFlicker>[1][]
+    | ((index: number) => Parameters<typeof useFlicker>[1])
+) => {
+  let i = 0
+  if (typeof config === 'function') {
+    for (let el of els) {
+      useFlicker(el, config(i))
+      i++
+    }
+  } else if (config instanceof Array) {
+    for (let el of els) {
+      invariant(config[i], 'Not enough configs in list')
+      i++
+      useFlicker(el, config[i])
+    }
+  } else {
+    for (let el of els) {
+      useFlicker(el, config)
+    }
+  }
+}
+
+export const lerp = (a: number, b: number, progress: number) => {
+  return a + (b - a) * progress
 }
